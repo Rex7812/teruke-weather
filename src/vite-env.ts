d91c68054e42c8062b27921b6af30b036798d8a1
Emmanuel Ukekor <62305790+Rex7812@users.noreply.github.com>
2026-02-27 04:16:39 +0100

    Create vite-env.ts

---
+++ src/vite-env.ts
@@ -0,0 +1,17 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_GEMINI_API_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_GEMINI_API_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
